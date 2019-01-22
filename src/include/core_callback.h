cde28fb02e9e02e33d15478949245dd51626e6f6
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-01-22 08:18:32 -0500

    incorporating pret names

---
+++ src/include/core_callback.h
@@ -0,0 +1,76 @@
+#ifndef POKEAGB_CALLBACK_H_
+#define POKEAGB_CALLBACK_H_
+
+#include "types.h"
+
+typedef void (*MainCallback)(void);
+typedef void (*IntrCallback)(void);
+typedef void (*IntrFunc)(void);
+
+/**
+ * Bits for the keypad;
+ */
+enum Keypad {
+    KEY_A = (1 << 0),
+    KEY_B = (1 << 1),
+    KEY_SELECT = (1 << 2),
+    KEY_START = (1 << 3),
+    KEY_RIGHT = (1 << 4),
+    KEY_LEFT = (1 << 5),
+    KEY_UP = (1 << 6),
+    KEY_DOWN = (1 << 7),
+    KEY_L = (1 << 8),
+    KEY_R = (1 << 9),
+
+    /** Directional pad mask */
+    KEY_DPAD = (KEY_UP | KEY_DOWN | KEY_LEFT | KEY_RIGHT),
+};
+
+
+struct Main
+{
+    /*0x000*/ MainCallback callback1;
+    /*0x004*/ MainCallback callback2;
+
+    /*0x008*/ MainCallback savedCallback;
+
+    /*0x00C*/ IntrCallback vblankCallback;
+    /*0x010*/ IntrCallback hblankCallback;
+    /*0x014*/ IntrCallback vcountCallback;
+    /*0x018*/ IntrCallback serialCallback;
+
+    /*0x01C*/ vu16 intrCheck;
+
+    /*0x020*/ u32 *vblankCounter1;
+    /*0x024*/ u32 vblankCounter2;
+
+    /*0x028*/ enum Keypad heldKeysRaw;           // held keys without L=A remapping
+    /*0x02A*/ enum Keypad newKeysRaw;            // newly pressed keys without L=A remapping
+    /*0x02C*/ enum Keypad heldKeys;              // held keys with L=A remapping
+    /*0x02E*/ enum Keypad newKeys;               // newly pressed keys with L=A remapping
+    /*0x030*/ enum Keypad newAndRepeatedKeys;    // newly pressed keys plus key repeat
+    /*0x032*/ enum Keypad keyRepeatCounter;      // counts down to 0, triggering key repeat
+    /*0x034*/ bool16 watchedKeysPressed; // whether one of the watched keys was pressed
+    /*0x036*/ u16 watchedKeysMask;       // bit mask for watched keys
+
+    /*0x038*/ struct OamData oamBuffer[128];
+
+    /*0x438*/ u8 state;
+
+    /*0x439*/ u8 oamLoadDisabled:1;
+    /*0x439*/ u8 inBattle:1;
+    /*0x439*/ u8 field_439_x4:1;
+};
+
+struct Main gMain;
+
+void SetMainCallback(MainCallback func);
+void SetMainCallback2(MainCallback func);
+void SetVBlankCallback(MainCallback func);
+void SetHBlankCallback(MainCallback func);
+void SetVCountCallback(IntrCallback callback);
+void SetSerialCallback(IntrCallback callback);
+
+
+
+#endif /* POKEAGB_CALLBACK_H_ */
---
+++ src/include/types.h
@@ -0,0 +1,20 @@
+#ifndef POKEAGB_TYPES_H_
+#define POKEAGB_TYPES_H_
+
+
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed int s32;
+typedef volatile u8 vu8;
+typedef volatile u16 vu16;
+typedef volatile u32 vu32;
+typedef volatile s8 vs8;
+typedef volatile s16 vs16;
+typedef volatile s32 vs32;
+
+
+#endif /* POKEAGB_TYPES_H_ */
