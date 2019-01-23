8661594c2e5110b663587cf39fc031e4a9f6e532
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2019-01-23 18:54:20 -0500

    remove VRAM buffer from use and remove help menu

--- patches/save.s
+++ patches/save.s
@@ -1,4 +1,4 @@
-// save game hooks
+// save game function redirections
 .org 0x080D9870
     ldr r2, =(HandleWriteSector|1)
     bx r2
@@ -14,13 +14,36 @@
     bx r1
 .pool
 
+// Disable read and write of sector 30 + 31
 .org 0x0815D80C
     bx lr
 .pool
 
 .org 0x080DA5E0
-    mov r0, #0xFf
+    mov r0, #0xFF
     bx lr
+
+// Disable the VRAM buffer being used for random screens
+.org 0x080F5190
+    mov r8, r8
+    mov r8, r8
+.pool
+
+.org 0x080F52A0
+    mov r8, r8
+    mov r8, r8
+.pool
+
+.org 0x0813BAA8
+    mov r8, r8
+    mov r8, r8
+.pool
+
+// Disable help menu
+.org 0x0813B8C2
+    .byte 0x1D, 0xE0
+.pool
+
 
 // table adjustments
 .org 0x080DA23C
